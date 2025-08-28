15b5fd37764a0d6e680379b8bd631f0975dbef87
SebMZI <mrgy.sebastien@gmail.com>
2025-08-28 22:26:15 +0200

    feat: create socketio server

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 import "./config/env.config.js";
 import express from "express";
-const app = express();
+import { createServer } from "node:http";
+import { Server } from "socket.io";
 
-app.listen(process.env.PORT, () => {
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("a user connected", socket);
+});
+
+server.listen(process.env.PORT, () => {
   console.log(`server is running on localhost:${process.env.PORT}`);
 });
