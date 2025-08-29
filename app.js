7ddbc2b1f9d3efbd1dd7471fe799a6b241d24bd5
SebMZI <mrgy.sebastien@gmail.com>
2025-08-29 12:18:58 +0200

    feat: implement AI and response to app.js server

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 import "./config/env.config.js";
 import express from "express";
+import cors from "cors";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
+import { generateResponse } from "./controllers/chat.controller.js";
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 
 io.on("connection", (socket) => {
-  console.log("a user connected", socket);
+  socket.on("chat message", async (msg) => {
+    const response = await generateResponse(msg);
+    if (response) {
+      io.to(socket.id).emit("chat message", response);
+    }
+  });
 });
 
 server.listen(process.env.PORT, () => {
