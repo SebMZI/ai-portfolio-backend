a4fb1940ebc67b41a45609358ecadadd77086d64
SebMZI <mrgy.sebastien@gmail.com>
2025-08-29 16:01:42 +0200

    feat: add client url to cors policy

--- app.js
+++ app.js
@@ -9,14 +9,14 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", process.env.CLIENT_URL],
   },
 });
 
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", process.env.CLIENT_URL],
   })
 );
 
